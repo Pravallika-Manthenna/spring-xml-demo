106851aebd3e8a3c7a778f6a2ce900a19ec7946e
Pravallika-Manthenna <51950359+Pravallika-Manthenna@users.noreply.github.com>
2019-07-22 13:52:46 +0530

    Update Movie.java

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -4,18 +4,19 @@ public class Movie {
   public Movie(Actor actor) {
     this.actor = actor;
   }
-
+//creating object for actor class
   Actor actor;
-
+//generating getter
   public Actor getActor() {
     return actor;
   }
 
 
-
+//generating setter
   public void setActor(Actor actor) {
     this.actor = actor;
   }
+  //method to display
   public void displayActor()
   {
     actor.display();
