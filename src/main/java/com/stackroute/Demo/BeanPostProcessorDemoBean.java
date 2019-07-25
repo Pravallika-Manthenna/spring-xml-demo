57f8253bd76ec1b579bbca9ea42c38e2b108000e
Pravallika-Manthenna <51950359+Pravallika-Manthenna@users.noreply.github.com>
2019-07-25 10:51:28 +0530

    Update BeanPostProcessorDemoBean.java

--- src/main/java/com/stackroute/Demo/BeanPostProcessorDemoBean.java
+++ src/main/java/com/stackroute/Demo/BeanPostProcessorDemoBean.java
@@ -4,12 +4,13 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
 public class BeanPostProcessorDemoBean implements BeanPostProcessor {
+  //bean post processor before initialization
   public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException
   {
     System.out.println("Called postProcessBeforeInitialization() for :" + beanName);
     return bean;
   }
-
+//bean post processor after initialization
   public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException
   {
     System.out.println("Called postProcessAfterInitialization() for :" + beanName);
